9196786f4541f3bcc041725d162eb7aee315de3f
initishsachdeva <nitishsachdeva39@gmail.com>
2024-04-07 01:05:15 +0530

    join demo

---
+++ pyspark-basics/spark-programs/15_Shuffle_Join_Demo/ShuffleJoinDemo.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from pyspark.sql import SparkSession
+
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+if __name__ == "__main__":
+    spark = SparkSession.builder \
+                    .appName("Sfuffle Join Demo") \
+                    .master("local[3]") \
+                    .getOrCreate()
+    
+    flight_time_df1 = spark.read.json("pyspark-basics/spark-programs/15_Shuffle_Join_Demo/data/d1/")
+    flight_time_df2 = spark.read.json("pyspark-basics/spark-programs/15_Shuffle_Join_Demo/data/d2/")
+
+    spark.conf.set("spark.sql.shuffle.partitions", 3)
+
+    join_expr = flight_time_df1.id == flight_time_df2.id
+    join_df = flight_time_df1.join(flight_time_df2, join_expr, "inner") \
+
+    join_df.collect()
+    input("press a key to stop...")
