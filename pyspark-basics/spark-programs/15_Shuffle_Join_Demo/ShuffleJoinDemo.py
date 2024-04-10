8bd6bd5c29d903f2e64cab8792f24244f41d198e
initishsachdeva <nitishsachdeva39@gmail.com>
2024-04-11 01:07:39 +0530

    shuffle and bucket join demo

--- pyspark-basics/spark-programs/15_Shuffle_Join_Demo/ShuffleJoinDemo.py
+++ pyspark-basics/spark-programs/15_Shuffle_Join_Demo/ShuffleJoinDemo.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import broadcast
+
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -17,7 +19,12 @@ if __name__ == "__main__":
     spark.conf.set("spark.sql.shuffle.partitions", 3)
 
     join_expr = flight_time_df1.id == flight_time_df2.id
-    join_df = flight_time_df1.join(flight_time_df2, join_expr, "inner") \
+    join_df = flight_time_df1.join(flight_time_df2, join_expr, "inner") 
+    
+    # when your dataframe is small you can use Broadcast join
+    # join_df = flight_time_df1.join(broadcast(flight_time_df2), join_expr, "inner") 
 
     join_df.collect()
     input("press a key to stop...")
+
+   
